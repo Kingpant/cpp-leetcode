ba307d9b9cc7242d1d7f78afa02d0916d4becd28
pond.khachen <pondryu1902@gmail.com>
2022-07-16 12:52:46 +0700

    🎉 first commit

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+    
+    }
+};
+
+int main()
+{
+    vector<int> problem {1,1,2};
+    return 0;
+}
